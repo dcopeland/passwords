169fe89e6f81d4a242500bb65c1d6df80f5da22b
Dan Copeland <dpc-github@turophile.net>
2022-02-26 17:59:15 -0500

    Add passwords#chars

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -4,4 +4,10 @@ class PasswordsController < ApplicationController
       Passphrase::Passphrase.new(languages: ["english"], number_of_words: rand(3) + 3).to_s
     end
   end
+
+  def chars
+    @passwords = (0..9).map do
+      Passgen::generate(length: (rand(13) + 8), symbols: true)
+    end
+  end
 end
